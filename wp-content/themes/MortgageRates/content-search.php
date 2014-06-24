9bd7982b231a6506ebbefd4ca4b88a54d0d51add
Mauricio Gamboa <mgamboa@mobiquityinc.com>
2014-06-24 17:31:19 -0600

    search page

---
+++ wp-content/themes/MortgageRates/content-search.php
@@ -0,0 +1,4 @@
+<section>
+  <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+  <?php the_excerpt(); ?>
+</section>
--- wp-content/themes/MortgageRates/functions.php
+++ wp-content/themes/MortgageRates/functions.php
@@ -34,6 +34,7 @@ if (!function_exists('morgageRates_setup')) :
       add_theme_support('post-thumbnails');
       set_post_thumbnail_size(150, 150);
       add_image_size('blog', 200, 200, true);
+      add_theme_support( 'html5', array( 'search-form' ) );
     }
   }
 endif; // morgageRates_setup
@@ -43,4 +44,34 @@ function get_related_author_posts() {
   global $authordata, $post;
   $authors_posts = get_posts( array( 'author' => $authordata->ID, 'post__not_in' => array( $post->ID ), 'posts_per_page' => 4 ) );
   return $authors_posts;
+}
+
+function get_search_results_counter() {
+  /* Search Total Count */
+  $allsearch = &new WP_Query("s=$s&showposts=-1");
+  $count = $allsearch->post_count;
+  wp_reset_query();
+
+  /* Number of posts per page and page number */
+  $num_of_posts = 2;
+  $pageNumber = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+  /* Showing the lower post value */
+  $n = ($pageNumber-1) * $num_of_posts;
+  $n = $n+1;
+
+  /* Showing the higher/highest post value */
+  $m = $pageNumber * $num_of_posts;
+  
+  if($m > $count){
+    // if m is bigger than the count var, it sets the
+    // highest value equal to the count, this is for the last page of results
+    $m = $count;
+  }
+
+  if($count > 0){
+    return '<span class="hide-xs">Showing '.$n.'-'.$m.' of '.$count.' Results</span>';
+  } else {
+    return '<span class="hide-xs">No Results</span>';
+  }
 }
